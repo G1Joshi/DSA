e5c88a9e94c994e09ac19004231fdf088c68b780
G1Joshi <joshijeevan98@gmail.com>
2021-01-27 07:45:00 +0530

    count of digits

---
+++ Mathematics/Count Digits.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int x)
+{
+    int count = 0;
+    while (x > 0)
+    {
+        x = x / 10;
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int number = 789;
+    cout << countDigits(number);
+    return 0;
+}
